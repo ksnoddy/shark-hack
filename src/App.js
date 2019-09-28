b8f8bbbb0c7af8bc2169ea78183fd21f4c6a14ac
Ken Snoddy <ksnoddy@wpi.edu>
2019-09-27 21:50:07 -0400

    add ilana

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hellow World!
+          Hello Ilana :D!
         </a>
       </header>
     </div>
